260d828e03f3476a2a0099e77e83f6b175f49928
Ari Lerner <arilerner@mac.com>
2009-04-04 21:23:50 -0700

    Added specs for reloadable

--- test/reloadable_test.rb
+++ test/reloadable_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
+class ReloadableTester
+  include Reloadable
+end
 
 class ReloadableTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "respond to reload_files!" do;assert ReloadableTester.respond_to?(:reload_files!);end
+  should "respond to require_reloadable_files" do;assert ReloadableTester.respond_to?(:require_reloadable_files);end
+  should "respond to reloadable_files" do;assert ReloadableTester.respond_to?(:reloadable_files);end
+  should "add to the reloadable files when require_reloadable_files" do
+    ReloadableTester.require_reloadable_files "dummy.rb"
+    assert ReloadableTester.reloadable_files.include?(File.expand_path("dummy.rb"))
   end
 end
